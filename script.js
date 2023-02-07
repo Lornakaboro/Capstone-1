8283aa399900c93aa28b3f1f825e008e89a4e575
Lornakaboro <lornakaboro@gmail.com>
2023-02-07 19:53:52 +0300

    Modify function to display speacialists dynamically

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function openDropdownItem(menuElement, menuLink) {
 
 const specialistData = [
   {
-    featuredImage: './images/Mary-Kaboro.jpg',
+    featuredImage: './images/speaker1.jpg',
     name: 'Dr Mary Kaboro pHD.',
     specialization: 'Senior Nutritionist',
     description:
@@ -29,57 +29,65 @@ const specialistData = [
 
   },
   {
-    featuredImage: './images/Lactation expert.jpg',
+    featuredImage: './images/speaker-2.jpeg',
     name: 'Lorna Nyawira',
     specialization: 'Lactation Expert',
     description:
     'Certified breastfeeding and lactation specialist.Authored the book Breastfeeding is Life 2009.',
   },
   {
-    featuredImage: './images/fitness -coach.jpg',
+    featuredImage: './images/speaker-3.jpg',
     name: 'Ian Kirii',
     specialization: 'Fitness and wellness coach',
     description:
     'Lifestyle and behaviour change specialist,excecutive director YOLO,Fitness center.',
   },
   {
-    featuredImage: './images/Senior dietician.jpg',
+    featuredImage: './images/speaker4.jpg',
     name: 'Denis Munyao',
     specialization: 'Senior Dietician,director Meal plans by Deno',
     description:
     'Nutrition specialist,founder of the campaign You are what you eat 2020.',
   },
   {
-    featuredImage: './images/pharmacist.jpg',
+    featuredImage: './images/speaker-5.jpg',
     name: 'Albert Johnson',
     specialization: 'Certified pharmacist',
     description:
     'Registered pharmacist specializing in  a broad spectrum of nutritional supplements.',
   },
   {
-    featuredImage: './images/Kevin-bahati.jpg',
-    name: 'Kevin Bahati',
+    featuredImage: './images/speaker-6.jpg',
+    name: 'Joy Bahati',
     specialization: 'Registered Nutritionist',
     description:
-    'Registered nutritionist specializing in nutrional assesments,diagonosis and evaluations.',
+    'Registered nutritionist specializing in nutritional assesments,diagonosis and evaluations.',
   },
 ];
 
-for (let i = 0; i < specialistData.length; i += 1) {
-  document.querySelector('.featured-specialists-container').innerHTML += ` 
-  <div class="specialist-content col-sm-12 col-md-6 row mb-4">
-          <img
-            src="${specialistData[i].featuredImage}"
-            alt=""
-            class="specialist-image col-4"
-          />
-          <div class="col-8">
-            <h4 class="specialist-name">${specialistData[i].name}</h4>
-            <p class="specialist-title">${specialistData[i].specialization}</p>
-            <p class="specialist-description">
-            ${specialistData[i].description}
-            </p>
+function generateSpecialistData(specialistData){
+  let data = '';
+  
+  for (let i = 0; i < specialistData.length; i += 1) {
+  
+    data += ` 
+    <div class="specialist-content">
+            <img
+              src="${specialistData[i].featuredImage}"
+              alt=""
+              class="specialist-image"
+            />
+            <div class="specialist-details">
+              <h4 class="specialist-name">${specialistData[i].name}</h4>
+              <p class="specialist-title">${specialistData[i].specialization}</p>
+              <p class="specialist-description">
+              ${specialistData[i].description}
+              </p>
+            </div>
           </div>
-        </div>
-    `;
+      `;
+  }
+  return data;
 }
+document.querySelector('.featured-specialists-container').innerHTML = generateSpecialistData(specialistData)
+
